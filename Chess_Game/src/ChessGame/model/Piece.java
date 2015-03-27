48fdee78c1988318f7f56e30ff44ebe8e5fdc0d4
lrbratton <l.bratton@hotmail.co.nz>
2015-03-27 17:40:04 +1300

    Piece Hierarchy

--- Chess_Game/src/ChessGame/model/Piece.java
+++ Chess_Game/src/ChessGame/model/Piece.java
@@ -3,19 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chess_game;
+package ChessGame.model;
 
 /**
  *
  * @author Laz Bratton
  */
 abstract public class Piece {
-    boolean alive;
-    boolean colour;
+    private Position position;
+    private boolean alive;
+    boolean colour; //True for black, false for white, until we implement GUI
     
-    Piece() {};
     
+    /**
+     * Constructor for Piece class
+     * 
+     * @param alive - designates whether a piece is active on the board or not 
+     */
     Piece(boolean alive) {
-        alive = this.alive;
+        this.alive = alive;
+        this.position = null;
+    }
+    
+    /**
+     * Getter method to return whether piece is active
+     * 
+     * @return alive 
+     */
+    public boolean isAlive() {
+        return alive;
+    }
+    
+    /**
+     * Sets the state of the piece, can be used when a piece is 
+     * taken by an opponent
+     * 
+     * @param aliveState - the new state of the piece 
+     */
+    public void setAlive(boolean aliveState) {
+        alive = aliveState;
+    }
+    
+    /**
+     * Gets the pieces place on the board
+     * 
+     * @return position on chess board 
+     */
+    public Position getPosition() {
+        return position;
+    }
+    
+    public void setPosition(Position position) {
+        this.position = position;
     }
 }
